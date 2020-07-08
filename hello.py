04d3f7e6fdf3a3ac33bc376e48ec7c9d2ed30232
Ronaldo Dantas <rondan1991@gmail.com>
2020-07-08 16:23:29 -0300

    inicio

---
+++ hello.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+root = Tk()
+
+myLabel1 = Label(root, text="Hello World")
+myLabel2 = Label(root, text="My name is Ronaldo")
+
+myLabel1.grid(row=0, column=0)
+myLabel2.grid(row=1, column=5)
+
+
+root.mainloop()
