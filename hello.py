b37186bca078044e71bf209a86807db24b81bf4b
Ronaldo Dantas <rondan1991@gmail.com>
2020-07-08 19:14:26 -0300

    botão

--- hello.py
+++ hello.py
@@ -2,11 +2,12 @@ from tkinter import *
 
 root = Tk()
 
-myLabel1 = Label(root, text="Hello World")#.grid(row=0, column=0)
-myLabel2 = Label(root, text="My name is Ronaldo")#.grid(row=1, column=5)
+def myClick():
+    myLabel = Label(root, text="Look! I clicked a Button!")
+    myLabel.pack()
 
-myLabel1.grid(row=0, column=0)
-myLabel2.grid(row=1, column=5)
+myButton = Button(root, text = "Click Me",command=myClick,fg="blue",bg="#000000")
+myButton.pack()
 
 
 root.mainloop()
